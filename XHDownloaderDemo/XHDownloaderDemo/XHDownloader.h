1c6f1752623fe4d8b14bc36be6ba8e3640277565
xinghunm <xinghunm@curacloudcorp.com>
2017-04-25 19:36:27 +0800

    add xhdownloader

---
+++ XHDownloaderDemo/XHDownloaderDemo/XHDownloader.h
@@ -0,0 +1,22 @@
+//
+//  XHDownloader.h
+//  XHDownloaderDemo
+//
+//  Created by xinghun meng on 25/04/2017.
+//  Copyright © 2017 xinghun meng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^XHDownloaderProgressBlock)(NSInteger expectedSize, NSInteger receivedSize, NSInteger speed);
+typedef void(^XHDownloaderCompletedBlock)( BOOL finished);
+
+
+@interface XHDownloader : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)downloadWithURL:(NSURL *)url
+               progress:(XHDownloaderProgressBlock)progressBlock
+              completed:(XHDownloaderCompletedBlock)completedBlock;
+@end
---
+++ XHDownloaderDemo/XHDownloaderDemo/XHDownloader.m
@@ -0,0 +1,69 @@
+//
+//  XHDownloader.m
+//  XHDownloaderDemo
+//
+//  Created by xinghun meng on 25/04/2017.
+//  Copyright © 2017 xinghun meng. All rights reserved.
+//
+
+#import "XHDownloader.h"
+
+@implementation XHDownloader
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t onceToken;
+    static XHDownloader *downloader;
+    dispatch_once(&onceToken, ^{
+        downloader = [[XHDownloader alloc]init];
+    });
+    return downloader;
+}
+
+- (void)downloadWithURL:(NSURL *)url
+               progress:(XHDownloaderProgressBlock)progressBlock
+              completed:(XHDownloaderCompletedBlock)completedBlock {
+    
+}
+
+#pragma mark NSURLSessionDataDelegate
+
+- (void)URLSession:(NSURLSession *)session
+          dataTask:(NSURLSessionDataTask *)dataTask
+didReceiveResponse:(NSURLResponse *)response
+ completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler {
+    
+    // Identify the operation that runs this task and pass it the delegate method
+//    MCDownloadOperation *dataOperation = [self operationWithTask:dataTask];
+//    
+//    [dataOperation URLSession:session dataTask:dataTask didReceiveResponse:response completionHandler:completionHandler];
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
+    
+    // Identify the operation that runs this task and pass it the delegate method
+//    MCDownloadOperation *dataOperation = [self operationWithTask:dataTask];
+//    
+//    [dataOperation URLSession:session dataTask:dataTask didReceiveData:data];
+}
+
+- (void)URLSession:(NSURLSession *)session
+          dataTask:(NSURLSessionDataTask *)dataTask
+ willCacheResponse:(NSCachedURLResponse *)proposedResponse
+ completionHandler:(void (^)(NSCachedURLResponse *cachedResponse))completionHandler {
+    
+    // Identify the operation that runs this task and pass it the delegate method
+//    MCDownloadOperation *dataOperation = [self operationWithTask:dataTask];
+//    
+//    [dataOperation URLSession:session dataTask:dataTask willCacheResponse:proposedResponse completionHandler:completionHandler];
+}
+
+#pragma mark NSURLSessionTaskDelegate
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    // Identify the operation that runs this task and pass it the delegate method
+//    MCDownloadOperation *dataOperation = [self operationWithTask:task];
+//    
+//    [dataOperation URLSession:session task:task didCompleteWithError:error];
+}
+
+@end
